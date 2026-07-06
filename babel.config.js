7edfae71d24d6a8ce0220a13d42aa1c893e7a374
MOUIZ HOUHOU <mouizbiskra@gmail.com>
2026-07-06 15:37:20 +0100

    Fix Prisma Neon config and Railway deployment

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      ['babel-preset-expo', { jsxImportSource: 'nativewind', reanimated: false }],
     ],
     plugins: [
       require('react-native-css-interop/dist/babel-plugin').default,
@@ -13,6 +13,7 @@ module.exports = function (api) {
           importSource: 'react-native-css-interop',
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
